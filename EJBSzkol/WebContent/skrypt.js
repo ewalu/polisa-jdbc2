ba1afab5f715463c2313bde1f961225305b2498a
ewalu <ewa_lugin@tlen.pl>
2018-02-11 14:18:14 +0100

    20180211 indeks musi byc w localhoscie

--- EJBSzkol/WebContent/skrypt.js
+++ EJBSzkol/WebContent/skrypt.js
@@ -3,19 +3,44 @@ app.controller('mojCtrl', function($scope, $http){
     const scope = $scope;
     scope.mojTekst = 'test';
 
+    scope.isSave = 'false';
+
+    scope.statusy=[
+        'ZATWIERDZONA',
+        'ZAWIESZONA',
+        'ROZWIAZANA'
+
+    ];
+
     scope.model = {
         numerPolisy : null,
         ubezpieczajacy : null,
-        sklaka : null
+        skladka : null//,
+        //signDate : null,
+        //statusPolisy : null
     }
 
-    scope.test = () => {
-        $http.post('http://localhost:8080/EJBSzkol/api/polisa',scope.model).
-        
-        then((response) => {
-            alert('sukces');
+
+    scope.zapiszPolise = () =>{
+        $http(
+            {
+                method: 'GET',
+                //params: scope.model,
+                url: 'api/polisa/create/'+scope.model.numerPolisy+'/'+scope.model.ubezpieczajacy+'/'+scope.model.skladka,
+                //params: {"numerPolisy": model.numerPolisy, "ubezpieczajacy": model.ubezpieczajacy, "skladka": model.skladka},
+                //data: scope.model,
+                headers: {'Content-Type': 'application/json '},
+                withCredentials: true
+            }
+        ).then(
+            (response) => {
+            isSave = true;
+           alert(`Poprawnie zapisano dane polisy. Id: ${response.data.id}`);
+           console.log('ewa666'+response);
         }, (response) => {
-            alert('bład');
-        })
+            alert('Błąd zapisu danych: ' + response);
+            console.log('ewa666'+response);
+        }
+    );
     }
 });
