df72f047c77b8d8c908513c76576676a4147f222
ewalu <ewa_lugin@tlen.pl>
2018-03-11 16:49:18 +0100

    20180311

---
+++ EJBSzkol/src/pl/edu/atena/biz/file/ZapiszDoJSON.java
@@ -0,0 +1,24 @@
+package pl.edu.atena.biz.file;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import pl.edu.atena.entities.Polisa;
+
+public class ZapiszDoJSON implements ZapiszDoPliku {
+	
+	public void zapisz(Polisa polisa) {
+		//ObjectMapper mapper = new ObjectMapper();
+		
+		 /*try {
+			 File file = new File("D:\\pliki\\polisa.json");
+
+
+			      } /*catch (JAXBException e) {
+				e.printStackTrace();
+			      }*/
+
+			}
+
+}
