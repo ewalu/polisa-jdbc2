81a4ecccdafc6a57827dda4ae635c5aee7108a6d
ewalu <ewa_lugin@tlen.pl>
2018-03-22 13:03:25 +0100

    json

--- EJBSzkol/src/pl/edu/atena/biz/file/ZapiszDoJSON.java
+++ EJBSzkol/src/pl/edu/atena/biz/file/ZapiszDoJSON.java
@@ -1,24 +1,43 @@
 package pl.edu.atena.biz.file;
 
 import java.io.File;
+import java.io.IOException;
 
-import javax.xml.bind.JAXBException;
+import javax.inject.Named;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import pl.edu.atena.entities.Polisa;
 
 public class ZapiszDoJSON implements ZapiszDoPliku {
 	
 	public void zapisz(Polisa polisa) {
-		//ObjectMapper mapper = new ObjectMapper();
-		
-		 /*try {
-			 File file = new File("D:\\pliki\\polisa.json");
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			// Convert object to JSON string and save into a file directly
+			//mapper.writeValue(new File("c:\\" + polisa.getNumerPolisy() + ".json"), polisa);
 
+			// Convert object to JSON string
+			String jsonInString = mapper.writeValueAsString(polisa);
+			System.out.println(jsonInString);
 
-			      } /*catch (JAXBException e) {
-				e.printStackTrace();
-			      }*/
+			// Convert object to JSON string and pretty print
+			jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(polisa);
+			System.out.println(jsonInString);
+			
+			
+			mapper.writerWithDefaultPrettyPrinter().writeValue(new File("D:\\pliki\\" + polisa.getNumerPolisy() + ".json"), polisa);
+			
 
-			}
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
